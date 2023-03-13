89b57e16c889f5cf7ef10ac88f9cd0be32d7b66b
PGK79 <pgk_nev@mail.ru>
2023-03-13 11:59:34 +0400

    Создан класс DTO с одним полем имени продукта

---
+++ src/main/java/com/example/jdbc_api/model/Product.java
@@ -0,0 +1,10 @@
+package com.example.jdbc_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private final String productName;
+}
