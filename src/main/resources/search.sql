25abc6e29dd25dcf1913c1dc07a49c90173f4359
PGK79 <pgk_nev@mail.ru>
2023-03-17 17:00:34 +0400

    убран делиметр точка с запятой

--- src/main/resources/search.sql
+++ src/main/resources/search.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT orders.product_name
 FROM tables_for_task_two.ORDERS orders
 JOIN tables_for_task_two.CUSTOMERS customer ON customer.id = orders.customer_id
-WHERE name = :name;
+WHERE name = :name
